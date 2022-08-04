7918015a0ac840cd236dbf1b19da8b72a1e599f8
adam-urban <adam.jan.urban@gmail.com>
2022-08-04 22:17:10 +0200

    Create Main class and add Words.txt file

---
+++ Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Choose difficulty number:");
+        System.out.println("1: Easy");
+        System.out.println("2: Hard");
+
+        Scanner scan = new Scanner(System.in);
+        while(!scan.hasNext("[12]")){
+            System.out.println("Wrong input. Choose again");
+        }
+
+        int i = scan.nextInt();
+        Difficulty difficulty;
+        if(i==1)    difficulty = Difficulty.EASY;
+        else difficulty = Difficulty.HARD;
+     
+        MemoryGame m = new MemoryGame(difficulty);
+
+        m.play();
+    }
+    
+}
